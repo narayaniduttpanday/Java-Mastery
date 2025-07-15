777d8741e78a782342b01df9edf3364ae225b8ed
Narayani Dutt Pandey <155388709+narayaniduttpanday@users.noreply.github.com>
2025-07-15 11:54:19 +0530

    Update Array04.java
    Added the code to enter the elements in the array using user input via Scanner class , and commented out the part where it was predefined.

--- Data Structures/Array/Array04.java
+++ Data Structures/Array/Array04.java
@@ -30,7 +30,16 @@ public class Array04 {
     }
 
     public static void main(String[] args) {
-        int arr[] = {54, 36, 85, 11, -9, 3333, -6, 0, 52, 75, 84, 2564};
+        
+        //int arr[] = {54, 36, 85, 11, -9, 3333, -6, 0, 52, 75, 84, 2564};
+        Scanner am = new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to enter in your array // Enter it's size);
+        int size = am.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the "+size+" elements in your array");
+        for(int i = 0;i<arr.length;i++){
+            arr[i]=am.nextInt();
+        }
         System.out.println("The second largest value is: " + findSecondMax(arr));
         System.out.println("The second smallest value is: " + findSecondMin(arr));
     }
